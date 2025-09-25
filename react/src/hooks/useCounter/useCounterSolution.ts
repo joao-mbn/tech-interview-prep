2765fb0e195acf4e066963308764f82497342d90
João Nascimento <joaombn97@gmail.com>
2025-09-25 09:21:37 -0300

    useCounterSolution

---
+++ react/src/hooks/useCounter/useCounterSolution.ts
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+export function useCounter(initialValue: number = 0) {
+  const [count, setCount] = useState(initialValue)
+  const increment = () => setCount(prev => ++prev)
+  const decrement = () => setCount(prev => --prev)
+  const reset = () => setCount(0)
+
+  return {
+    count,
+    increment,
+    decrement,
+    reset
+  }
+}
