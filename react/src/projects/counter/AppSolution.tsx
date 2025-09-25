20f194533410e6966997c022f93e0f0b4daaf5df
João Nascimento <joaombn97@gmail.com>
2025-09-25 09:24:20 -0300

    Counter solution

---
+++ react/src/projects/counter/AppSolution.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [counter, setCounter] = useState(0)
+  return (
+    <div className="counter-app">
+      <h1>Counter Challenge</h1>
+      <p>Click the button to increment the counter</p>
+      <div className="counter-display">
+        <span className="count">{counter}</span>
+      </div>
+      <button className="increment-btn" onClick={() => setCounter(prev => ++prev)}>
+        Click me!
+      </button>
+      <button className="reset-btn" onClick={() => setCounter(0)}>
+        Reset
+      </button>
+    </div>
+  )
+}
+
+export default App
